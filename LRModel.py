d81c5977bf4b3beb768c7dbcbdcf7d0e9ec37424
John Strenio <johnstrenio@gmail.com>
2021-08-02 13:08:14 -0700

    keras nonlin model tested in 3D, integrated into compas model, still needs tuning

--- LRModel.py
+++ LRModel.py
@@ -1,6 +1,10 @@
 import numpy
 import matplotlib.pyplot as plt
 import pandas as pd
+import tensorflow as tf 
+from tensorflow import keras 
+from tensorflow.keras import layers, models
+from tensorflow.keras.models import Model
 from sklearn import preprocessing, linear_model
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error, r2_score
@@ -94,7 +98,66 @@ class Model:
         # Normal linear regression model
 
         # returns build_model's return values (X_test, y_test) for further testing
-        return self.build_model(result, y)
+        return self.build_nonlin_model(result, y)
+
+    def build_nonlin_model(self, X, y):
+        print("Spliting dataset 80/20...")
+        X_train, X_test, y_train, y_test = train_test_split(
+            X, y, test_size=0.2, random_state=0
+        )
+        print(X_train.shape, X_test.shape)
+        print("Training...")
+
+        # hyperparameters
+        lr = 0.001
+        n_epochs = 100 
+        n_att = X_train.shape[1]
+        print("n_attributes (model input size): " + str(n_att))
+        n_batch = n_att * 10
+
+        # define model 
+        self.model = models.Sequential()
+        self.model.add(layers.Dense(100, activation='relu', input_dim=n_att))
+        self.model.add(layers.Dense(50, activation='relu'))
+        self.model.add(layers.Dense(1))
+        self.model.summary()
+
+        opt = keras.optimizers.Adam(learning_rate=lr)
+        self.model.compile(loss='mse', optimizer=opt, metrics=['mse', 'mae'])
+
+        # train model
+        history = self.model.fit(X_train, y, epochs=n_epochs, batch_size=n_batch, verbose=2, validation_split=0.2)
+
+        # validate model
+        preds = []
+        for i in range(len(X_test)):
+            # grab a sample and compas score
+            input_sample = X_test.iloc[[i]]
+
+            # convert to np array and add batch dimension for model input
+            input_sample = numpy.asarray(input_sample)
+            input_sample = numpy.expand_dims(input_sample, axis=0)
+            #input_sample = numpy.squeeze(input_sample, axis=1)
+            if i == 1:
+                print('val sample shape:')
+                print(input_sample.shape)
+
+            # predict and get metrics
+            pred = self.model.predict(input_sample)[0][0]
+            preds.append(pred)
+
+        print("Val MSE: %.2f" % mean_squared_error(y_test, preds))
+        print("Val R2 score:", r2_score(y_test, preds))
+        # print("Model train score:", self.model.score(X_train, y_train))
+        # print("Model test score:", self.model.score(X_test, y_test))
+        # # The mean squared error
+        # print("MSE Train: %.2f" % mean_squared_error(y_train, x_pred))
+        # print("MSE Test: %.2f" % mean_squared_error(y_test, y_pred))
+        # # The coefficient of determination: 1 is perfect prediction
+        # print("Coefficient of determination: %.2f" % r2_score(y_test, y_pred))
+        # print()
+
+        return X_test, y_test
 
     def build_model(self, X, y):
         print("Spliting dataset 80/20...")
@@ -132,6 +195,8 @@ class Model:
     # TODO: generalize this to be able to work with any attribute
     def score_attribute(self, sample, d_score): # should be same input shape as model e.g. X_test
         sample = numpy.asarray(sample)[0]
+        #print('sample dims: ' + str(sample.shape))
+
         print("compas score: " + str(d_score))
 
         race_scores = []
@@ -141,7 +206,10 @@ class Model:
             for j in range(10, 14, 1):
                 sample[j] = 0
             sample[i] = 1
-            race_scores.append(self.model.predict([sample])[0][0])
+            model_input_sample = numpy.expand_dims(sample, axis=0)
+            #model_input_sample = numpy.squeeze(model_input_sample)
+            #print('new dims: ' + str(model_input_sample.shape))
+            race_scores.append(self.model.predict(model_input_sample)[0][0])
             print('race ' + str(i) + ' score: ' + str(race_scores[i]))
         print('---------------------------')
 
